df1f7afb63f8b331110ca1f9ef90d4b22cef06b8
ShiarheiYakubenka <yakubich.s@gmail.com>
2018-05-19 12:46:46 +0300

    new commit1

--- src/Runner.java
+++ src/Runner.java
@@ -12,23 +12,23 @@ public class Runner {
 
         int[] lens = new int[10_000_000];
 /*
+
         //Равномерно распределенные с 100000 уникальных длин
         Random random = new Random();
         for (int i = 0; i < lens.length; i++ ) {
             lens[i] = 1 + random.nextInt(100000);
         }
-*/
         //Упорядочены по возрастанию
         for (int i = 0; i < lens.length; i++ ) {
             lens[i] = i + 1;
         }
-/*
+
         //Уполрядочены по убыванию
         for (int i = lens.length -1; i >= 0; i-- ) {
             lens[i] = i + 1;
         }
 
-
+*/
         //Почти упорядоченный
         for (int i = 0; i < 9_000_000; i++ ) {
             lens[i] = i + 1;
@@ -38,7 +38,7 @@ public class Runner {
             lens[i] = 9_000_001 + random.nextInt(1_000_000);
         }
 
-
+/*
         //С уникальными длинами
         for (int i = 0, k = 1; i < lens.length; k++) {
             for (int j = 0; j < k && i < lens.length; j++) {
@@ -61,7 +61,8 @@ public class Runner {
 
 
 
-        long start = System.nanoTime();
+        //Лист
+        //Заполнение
         for (int len : lens) {
             NumLen numLen = new NumLen(len);
             int index = Collections.binarySearch(list, numLen);
@@ -73,20 +74,21 @@ public class Runner {
 
         }
 
-
-        long time = System.nanoTime() - start;
-
+        long start = System.nanoTime();
+        //Пересортировка
         Collections.sort(list, new NumLenComparator());
 
 
+        long time = System.nanoTime() - start;
         System.out.println(list.size());
         System.out.println(time / 1000);
 
 
 
 /*
-
+        //TrySet
         long start = System.nanoTime();
+        //Заполнение
         for (int len : lens) {
             NumLen numLen = new NumLen(len);
             sortedSetNumLen.add(numLen);
@@ -94,14 +96,16 @@ public class Runner {
         }
 
         long time = System.nanoTime() - start;
-
+        //Пересортировка
         List<NumLen> list1 = new ArrayList<>(sortedSetNumLen);
         Collections.sort(list1, new NumLenComparator());
         System.out.println(time / 1000);
 
 
 
+        //HashSet
         long start = System.nanoTime();
+        //Заполнение
         for (int len : lens) {
             NumLen numLen = new NumLen(len);
             setNumLen.add(numLen);
@@ -110,6 +114,7 @@ public class Runner {
 
 
         long time = System.nanoTime() - start;
+        //Пересортировка
         List<NumLen> list1 = new ArrayList<>(setNumLen);
         Collections.sort(list1, new NumLenComparator());
 
